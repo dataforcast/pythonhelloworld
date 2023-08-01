e8db7483d0108404ffd1fec7a237a9d221675021
mynewfbt <mynewfbt@gmail.com>
2023-08-01 16:50:58 +0200

    First commit

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from core import hello
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return hello.hello_world()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
