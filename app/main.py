cea093457a78c35fa3eb44bc6cc38da428dda76b
mynewfbt <mynewfbt@gmail.com>
2023-08-01 19:40:58 +0200

    First commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from core import hello
+import os
 
+# Port is retrieved from environment
+PORT = int(os.environ.get('PORT', 80))
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,4 +11,4 @@ def hello_world():
     return hello.hello_world()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=PORT)
